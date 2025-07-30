58dfb2af35d8b91626b8353f5e7a9096a025e7b1
JXPJXT <samjj317@gmail.com>
2025-07-30 13:02:42 +0530

    v14

---
+++ 1.Arrays/Easy/Q15LeadersInArr.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Q15LeadersInArr {
+    public static void main(String[] args) {
+        int[] arr = {16, 17, 4, 3, 5, 2};
+        findLeaders(arr);
+    }
+    static void findLeaders(int[] arr){
+        ArrayList<Integer> li = new ArrayList<>();
+        int rightMax=Integer.MIN_VALUE;
+        for(int i=arr.length-1;i>=0;i--){
+            if(arr[i]>rightMax){
+                li.add(arr[i]);
+                rightMax=arr[i];
+            }
+        }
+        Collections.reverse(li);
+        System.out.println("Leaders in the array are: " + li);
+    }
+}
