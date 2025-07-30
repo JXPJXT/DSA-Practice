6cfa24121db19a7f00c49789a6ccfee6901e9123
JXPJXT <samjj317@gmail.com>
2025-07-30 13:03:38 +0530

    v15

--- 1.Arrays/Easy/Q15LeadersInArr.java
+++ 1.Arrays/Easy/Q15LeadersInArr.java
@@ -1,4 +1,8 @@
 import java.util.*;
+//Find Leader in an array 
+//An element is a leader if it is greater than all the elements to its right side.
+    
+
 public class Q15LeadersInArr {
     public static void main(String[] args) {
         int[] arr = {16, 17, 4, 3, 5, 2};
